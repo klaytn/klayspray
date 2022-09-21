2de128812366a127504a7909d6cbb0441ff266f5
odg0318 <odg0318@gmail.com>
2022-09-21 16:14:52 +0900

    Update output

--- terraform/aws/private-layer1/outputs.tf
+++ terraform/aws/private-layer1/outputs.tf
@@ -1,30 +1,3 @@
-output "vpc_id" {
-  description = "Created VPC's ID"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnet_ids" {
-  description = "Created public subnets' IDs"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnet_ids" {
-  description = "Created private subnets' IDs"
-  value       = module.vpc.private_subnets
-}
-
-output "cn" {
-  value = module.layer1.cn
-}
-
-output "pn" {
-  value = module.layer1.pn
-}
-
-output "en" {
-  value = module.layer1.en
-}
-
-output "monitor" {
-  value = module.layer1.monitor
+output "layer1" {
+  value = module.layer1
 }
