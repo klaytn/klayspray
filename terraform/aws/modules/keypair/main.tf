073ed6ac8b701af610ebe9185750f6332f7acf69
odg0318 <odg0318@gmail.com>
2022-09-21 14:32:05 +0900

    Add == true for code consistency

--- terraform/aws/modules/keypair/main.tf
+++ terraform/aws/modules/keypair/main.tf
@@ -1,12 +1,12 @@
 resource "tls_private_key" "this" {
-  count = var.create_aws_key_pair ? 1 : 0
+  count = var.create_aws_key_pair == true ? 1 : 0
 
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
 resource "aws_key_pair" "this" {
-  count = var.create_aws_key_pair ? 1 : 0
+  count = var.create_aws_key_pair == true ? 1 : 0
 
   key_name   = var.name
   public_key = tls_private_key.this[0].public_key_openssh
@@ -15,7 +15,7 @@ resource "aws_key_pair" "this" {
 }
 
 resource "local_sensitive_file" "this" {
-  count = var.create_aws_key_pair ? 1 : 0
+  count = var.create_aws_key_pair == true ? 1 : 0
 
   content         = tls_private_key.this[0].private_key_openssh
   filename        = var.ssh_private_key_path
