ad62481342f902afb09a17a8460397e722689b73
jaeeuncho34 <jaylin.cho@klaytn.foundation>
2025-03-12 17:07:06 +0900

    update kaia version

--- terraform/gcp/private-layer1/data.tf
+++ terraform/gcp/private-layer1/data.tf
@@ -3,6 +3,11 @@ data "google_compute_zones" "this" {
 }
 
 data "google_compute_image" "this" {
+<<<<<<< HEAD
   family  = "centos-stream-9"
   project = "centos-cloud"
+=======
+  family  = "rocky-linux-9"
+  project = "rocky-linux-cloud"
+>>>>>>> d2097a0 (update kaia version)
 }
